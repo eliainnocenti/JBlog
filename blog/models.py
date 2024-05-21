0b8c25dae17c9145c5c0e2bfeaa2d41f1f31967f
Elia Innocenti <elia.innocenti@edu.unifi.it>
2024-05-21 14:14:45 +0200

    Template directories created

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,13 @@
 
 from django.db import models
 from django.utils import timezone
-# import user
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(default=timezone.now)
-    # add author field
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
